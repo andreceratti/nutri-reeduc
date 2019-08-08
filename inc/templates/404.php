a366d4b93f4517ea75d4245832753f1f16337899
Andre Ceratti Rocha <andre.ceratti.rocha@gmail.com>
2019-08-08 01:31:33 -0300

    Adicionado os arquivos enviados para mim

--- inc/templates/404.php
+++ inc/templates/404.php
@@ -1,2 +1,11 @@
-<center><h1>Erro 404</h1>
-<h3>Pagina não encontrada</h3></center>
+<div class="container-fluid">
+		<div class="content-image">
+			<div class="not-found">
+				<div class="text">
+					<h2>Página não encontrada</h2>
+					<p>Já que você não achou o que estava procurando, posso te sugerir outra página?</p>
+					<p>Clique <a href="">aqui</a> para ler sobre "André você consegue colocar o titulo do post aqui?"</p>
+				</div>				
+			</div>			
+		</div>		
+	</div>
